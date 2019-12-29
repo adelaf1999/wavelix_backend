cd2a51d59564f1343a7797eb19218b2617a3295a
user <adel_abouf@outlook.com>
2019-12-29 23:18:00 +0200

    changed get categories response in store_user.rb

--- app/models/store_user.rb
+++ app/models/store_user.rb
@@ -20,9 +20,21 @@ class StoreUser < ApplicationRecord
 
                 parent_category = {name: category.name, id: category.id, parent_id: category.parent_id}
 
+                if category.products.length > 0
+
+                    parent_category[:has_products] = true
+
+                else
+
+                    parent_category[:has_products] = false
+
+                end
+
                 child_categories = category.subcategories
 
                 if child_categories.length > 0
+
+                    parent_category[:has_subcategories] = true
 
                     parent_category_subcategories = get_child_subcategories(child_categories, category)
 
@@ -31,6 +43,8 @@ class StoreUser < ApplicationRecord
                     categories.push(parent_category)
 
                 else
+
+                    parent_category[:has_subcategories] = false
 
                     categories.push(parent_category)
 
@@ -53,11 +67,24 @@ class StoreUser < ApplicationRecord
 
         subcategories.each do |subcategory|
 
+            new_parent_category = { name: subcategory.name, id: subcategory.id, parent_id: subcategory.parent_id }
+
+
+            if subcategory.products.length > 0
+
+                new_parent_category[:has_products] = true
+
+            else
+
+                new_parent_category[:has_products] = false
+
+            end
+
             child_categories = subcategory.subcategories
 
             if child_categories.length > 0
 
-                new_parent_category = { name: subcategory.name, id: subcategory.id, parent_id: subcategory.parent_id }
+                new_parent_category[:has_subcategories] = true
 
                 new_parent_subcategories = get_child_subcategories(child_categories, subcategory)
 
@@ -67,11 +94,9 @@ class StoreUser < ApplicationRecord
 
             else
 
-                categories.push({
-                    name: subcategory.name,
-                    id: subcategory.id,
-                    parent_id: subcategory.parent_id
-                })
+                new_parent_category[:has_subcategories] = false
+
+                categories.push(new_parent_category)
 
             end
 
