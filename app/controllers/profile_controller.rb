a053ffa9bf1948fccfe241e1488c0438c30e8adc
user <adel_abouf@outlook.com>
2020-01-24 19:12:45 +0200

    send isVerified flag for view my profile for store

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -14,6 +14,7 @@ class ProfileController < ApplicationController
       @profile_data[:store_name] = store_user.store_name
       @profile_data[:store_address] = store_user.store_address
       @profile_data[:store_number] = store_user.store_number
+      @profile_data[:isVerified] = store_user.verified?
 
       # common profile data
       profile = current_user.profile
