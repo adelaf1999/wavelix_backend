4349f589afa71ab170a5e52cb63230740af87e35
user <adel_abouf@outlook.com>
2020-04-24 12:29:58 +0300

    unfollow user

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -46,6 +46,62 @@ class FollowController < ApplicationController
 
       @success = current_user.unfollow(user)
 
+      if @success
+
+        is_public = profile.public_account?
+
+        if is_public
+
+          @follow_relationships = get_follow_relationships(user)
+
+        else
+
+          @profile_data = {}
+
+          username = user.username
+          profile_picture = profile.profile_picture.url
+          profile_bio = profile.profile_bio
+
+          @profile_data[:is_following] = false
+          @profile_data[:user_type] = user.user_type
+          @profile_data[:username] = username
+          @profile_data[:profile_picture] = profile_picture
+          @profile_data[:profile_bio] = profile_bio
+          @profile_data[:is_private] = true
+
+          follow_relationships = get_follow_relationships(user)
+
+          followers = follow_relationships[:followers]
+
+          following = follow_relationships[:following]
+
+          placeholder_followers = []
+
+          placeholder_following = []
+
+          for i in 1..followers.count do
+
+            placeholder_followers.push("placeholder#{i}")
+
+          end
+
+          for i in 1..following.count do
+
+            placeholder_following.push("placeholder#{i}")
+
+          end
+
+          follow_relationships[:followers] = placeholder_followers
+
+          follow_relationships[:following] = placeholder_following
+
+          @profile_data[:follow_relationships] = follow_relationships
+
+        end
+
+
+      end
+
     else
 
       @success = false
--- app/views/follow/unfollow.json.rabl
+++ app/views/follow/unfollow.json.rabl
@@ -1,3 +1,11 @@
 if @success != nil
     node(:success) { @success }
 end
+
+if @follow_relationships != nil
+    node(:follow_relationships) {  @follow_relationships }
+end
+
+if @profile_data != nil
+    node(:profile_data) { @profile_data  }
+end
