84580e9281f76d3d15c2bfb324c1ab4924246ddb
user <adel_abouf@outlook.com>
2020-06-01 14:15:50 +0300

    day belongs to schedule

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,6 +1,6 @@
 class Day < ApplicationRecord
 
-  belongs_to :store_user
+  belongs_to :schedule
 
   enum week_day: { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }
 
