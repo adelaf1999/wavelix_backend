669ca400b1e5155f16dc5f6d99847ebc0dfe44f8
user <adel_abouf@outlook.com>
2020-06-02 12:41:07 +0300

    updated blocked countries list

--- app/controllers/concerns/RegistrationHelper.rb
+++ app/controllers/concerns/RegistrationHelper.rb
@@ -2,7 +2,7 @@ module RegistrationHelper
 
   def is_country_blocked?(country_code)
 
-    blocked_countries = ['IL']
+    blocked_countries = ['IL', 'AF', 'AL', 'BA', 'CD', 'CG', 'CI', 'CU', 'IR', 'IQ', 'KP', 'LR', 'LY', 'MM', 'NG', 'PK', 'SO', 'SS', 'SD', 'SY', 'VE', 'YE', 'ZW']
 
     blocked_countries.include?(country_code)
 
