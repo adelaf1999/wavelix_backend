e2b15c70b1514fda8cddc0fceea5d09bf0bdb493
user <adel_abouf@outlook.com>
2020-11-18 12:42:03 +0200

    If current_user is blank try autheticating user with access_token, client and uid in home_channel and if true start_stream in home_channel.rb

--- app/channels/home_channel.rb
+++ app/channels/home_channel.rb
@@ -1,13 +1,38 @@
 class HomeChannel < ApplicationCable::Channel
 
+
+  def start_stream(user_id)
+
+    stream_from "home_channel_#{user_id}"
+
+  end
+
   def subscribed
 
     if current_user.blank?
 
-      reject
+      access_token = params[:access_token]
+
+      client = params[:client]
+
+      uid = params[:uid]
+
+      user = User.find_by_uid(uid)
+
+      if user != nil && user.valid_token?(access_token, client)
+
+        start_stream(user.id)
+
+      else
+
+        reject
+
+      end
+
 
     else
-      stream_from "home_channel_#{current_user.id}"
+
+      start_stream(current_user.id)
 
     end
 
