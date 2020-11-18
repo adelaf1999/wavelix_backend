36b6e0c80db57aa411d86dcde73c3e4ab5e58c7c
user <adel_abouf@outlook.com>
2020-11-18 16:23:15 +0200

    If current_user and current_employee is blank try autheticating user with access_token, client and uid in category products channel

--- app/channels/category_products_channel.rb
+++ app/channels/category_products_channel.rb
@@ -1,38 +1,24 @@
 class CategoryProductsChannel < ApplicationCable::Channel
 
-  def subscribed
+  def start_stream(current_user, current_employee)
 
-    if  current_user.blank? && current_employee.blank?
+    category_id = params[:category_id]
 
-      reject
-
-    else
-
-      category_id = params[:category_id]
-
-      if !category_id.blank?
+    if !category_id.blank?
 
 
-        if !current_user.blank?
+      if !current_user.blank?
 
 
-          if current_user.store_user?
+        if current_user.store_user?
 
-            store_user = StoreUser.find_by(id: current_user.id)
+          store_user = StoreUser.find_by(id: current_user.id)
 
-            category = store_user.categories.find_by(id: category_id)
+          category = store_user.categories.find_by(id: category_id)
 
-            if category != nil
+          if category != nil
 
-              stream_from "category_#{category.id}_products_store_user_#{store_user.id}"
-
-            else
-
-              reject
-
-            end
-
-
+            stream_from "category_#{category.id}_products_store_user_#{store_user.id}"
 
           else
 
@@ -41,26 +27,86 @@ class CategoryProductsChannel < ApplicationCable::Channel
           end
 
 
-        elsif !current_employee.blank?
+
+        else
+
+          reject
+
+        end
 
 
-          if current_employee.has_roles?(:product_manager)
-
-            store_user = current_employee.store_user
-
-            category = store_user.categories.find_by(id: category_id)
-
-            if category != nil
-
-              stream_from "category_#{category.id}_products_store_user_#{store_user.id}"
-
-            else
-
-              reject
-
-            end
+      elsif !current_employee.blank?
 
 
+        if current_employee.has_roles?(:product_manager)
+
+          store_user = current_employee.store_user
+
+          category = store_user.categories.find_by(id: category_id)
+
+          if category != nil
+
+            stream_from "category_#{category.id}_products_store_user_#{store_user.id}"
+
+          else
+
+            reject
+
+          end
+
+
+
+        else
+
+          reject
+
+        end
+
+      end
+
+    else
+
+      reject
+
+    end
+
+  end
+
+  def subscribed
+
+    if  current_user.blank? && current_employee.blank?
+
+      access_token = params[:access_token]
+
+      client = params[:client]
+
+      uid = params[:uid]
+
+      is_user = params[:is_user]
+
+      if !access_token.blank? && !client.blank? && !uid.blank? && !is_user.blank?
+
+        if is_user
+
+          user = User.find_by_uid(uid)
+
+          if user != nil && user.valid_token?(access_token, client)
+
+            start_stream(user, current_employee)
+
+          else
+
+            reject
+
+          end
+
+        else
+
+          employee = Employee.find_by_uid(uid)
+
+          if employee != nil && employee.valid_token?(access_token, client)
+
+            start_stream(current_user, employee)
 
           else
 
@@ -76,6 +122,11 @@ class CategoryProductsChannel < ApplicationCable::Channel
 
       end
 
+
+
+    else
+
+      start_stream(current_user, current_employee)
 
     end
 
