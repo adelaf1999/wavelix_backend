7a8fee344e5cf64858e81a6eccc8be6d1fb3d310
user <adel_abouf@outlook.com>
2020-11-18 14:23:20 +0200

    If current_employee is blank try autheticating user with access_token, client and uid in employee_channel.rb

--- app/channels/employee_channel.rb
+++ app/channels/employee_channel.rb
@@ -1,15 +1,39 @@
 class EmployeeChannel <  ApplicationCable::Channel
 
+
+  def start_stream(current_employee)
+
+    stream_from "employee_channel_#{current_employee.id}"
+
+  end
+
   def subscribed
 
     if current_employee.blank?
 
-      reject
+
+      access_token = params[:access_token]
+
+      client = params[:client]
+
+      uid = params[:uid]
+
+      employee = Employee.find_by_uid(uid)
+
+      if employee != nil && employee.valid_token?(access_token, client)
+
+        start_stream(employee)
+
+      else
+
+        reject
+
+      end
+
 
     else
 
-      stream_from "employee_channel_#{current_employee.id}"
-
+      start_stream(current_employee)
 
     end
 
