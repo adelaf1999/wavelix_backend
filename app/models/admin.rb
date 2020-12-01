253fc0657e8ab7d433f8c26430ba1d9e47884526
user <adel_abouf@outlook.com>
2020-12-01 12:51:32 +0200

    Removed tiemout configuration in devise.rb and module in amdin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,7 +5,7 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :rememberable, :trackable, :timeoutable, :validatable, :lockable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :lockable
 
   include DeviseTokenAuth::Concerns::User
 
