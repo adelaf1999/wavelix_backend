cb8e1ac9e7f4f88ca5482a2e35f609f5be30a730
user <adel_abouf@outlook.com>
2020-12-18 15:35:48 +0200

    Created driver accounts channel

---
+++ app/channels/driver_accounts_channel.rb
@@ -0,0 +1,32 @@
+class DriverAccountsChannel < ApplicationCable::Channel
+
+
+  def subscribed
+
+    access_token = params[:access_token]
+
+    client = params[:client]
+
+    uid = params[:uid]
+
+    admin = Admin.find_by_uid(uid)
+
+    if admin != nil && admin.valid_token?(access_token, client)
+
+      stream_from 'driver_accounts_channel'
+
+    else
+
+      reject
+
+    end
+
+  end
+
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+
+end
