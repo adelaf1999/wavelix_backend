562f94c9a3ea1f9a78807e63fd2e7d2abf5776c0
Adel <adelwaboufakher@gmail.com>
2020-12-30 11:46:57 +0200

    Created the post cases channel

---
+++ app/channels/post_cases_channel.rb
@@ -0,0 +1,30 @@
+class PostCasesChannel <  ApplicationCable::Channel
+
+  def subscribed
+
+    access_token = params[:access_token]
+
+    client = params[:client]
+
+    uid = params[:uid]
+
+    admin = Admin.find_by_uid(uid)
+
+    if admin != nil && admin.valid_token?(access_token, client)
+
+      stream_from 'post_cases_channel'
+
+    else
+
+      reject
+
+    end
+
+  end
+
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+end
