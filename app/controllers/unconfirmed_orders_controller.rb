77342e6db660a61b7b921561de53efcf4189f59d
Adel <adelwaboufakher@gmail.com>
2021-01-15 13:26:09 +0200

    Added countries filters and time exceeded filters in unconfirmed_orders#index

--- app/controllers/unconfirmed_orders_controller.rb
+++ app/controllers/unconfirmed_orders_controller.rb
@@ -2,6 +2,8 @@ class UnconfirmedOrdersController < ApplicationController
 
   include AdminHelper
 
+  include CountriesHelper
+
   before_action :authenticate_admin!
 
   def index
@@ -32,6 +34,29 @@ class UnconfirmedOrdersController < ApplicationController
                                  })
 
       end
+
+
+      @countries = get_countries
+
+      @time_exceeded_filters = {
+          30 => 'More than 30 minutes',
+          60 => 'More than 1 hour',
+          180 => 'More than 3 hours',
+          360 => 'More than 6 hours',
+          540 => 'More than 9 hours',
+          720 => 'More than 12 hours',
+          900 => 'More than 15 hours',
+          1440 => 'More than 1 day',
+          4320 => 'More than 3 days',
+          10080 => 'More than 7 days',
+          14400 => 'More than 10 days',
+          20160 => 'More than 14 days',
+          28800 => 'More than 20 days',
+          43200 => 'More than 30 days',
+          64800 => 'More than 45 days',
+          86400 => 'More than 60 days'
+      }
+
 
     end
 
--- app/views/unconfirmed_orders/index.json.rabl
+++ app/views/unconfirmed_orders/index.json.rabl
@@ -1,3 +1,12 @@
+if @time_exceeded_filters != nil
+    node(:time_exceeded_filters) { @time_exceeded_filters }
+end
+
 if @unconfirmed_orders != nil
     node(:unconfirmed_orders) { @unconfirmed_orders }
+end
+
+
+if @countries != nil
+    node(:countries) { @countries }
 end
