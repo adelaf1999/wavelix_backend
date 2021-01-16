3780870bf7da61c80816663445586702909a8429
Adel <adelwaboufakher@gmail.com>
2021-01-16 11:10:30 +0200

    Created unconfirmed orders channel

---
+++ app/channels/unconfirmed_orders_channel.rb
@@ -0,0 +1,37 @@
+class UnconfirmedOrdersChannel < ApplicationCable::Channel
+
+  def subscribed
+
+    access_token = params[:access_token]
+
+    client = params[:client]
+
+    uid = params[:uid]
+
+    admin = Admin.find_by_uid(uid)
+
+    if admin != nil && admin.valid_token?(access_token, client)
+
+      if admin.has_roles?(:root_admin, :order_manager)
+
+        stream_from 'unconfirmed_orders_channel'
+
+      else
+
+        reject
+
+      end
+
+    else
+
+      reject
+
+    end
+
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+end
