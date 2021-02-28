f8c024e0f6c98a11c8565ec9597bb0bee4836fbc
Adel <adelwaboufakher@gmail.com>
2021-02-28 17:15:40 +0200

    Refactored calculate_our_earning_driver method in CommissionHelper.rb

--- app/controllers/concerns/CommissionHelper.rb
+++ app/controllers/concerns/CommissionHelper.rb
@@ -1,17 +1,31 @@
 module CommissionHelper
 
 
-  def calculate_our_earning_driver(net_amount)
-
-    net_amount * 0.1
-
-  end
-
   def calculate_our_earning_store(net_amount)
 
     net_amount * 0.1
 
   end
+
+  def calculate_our_earning_driver(net_amount)
+
+    if net_amount <= 10
+
+      net_amount * 0.25
+
+    elsif net_amount > 10 && net_amount <= 100
+
+      net_amount * 0.15
+
+    else
+
+      net_amount * 0.10
+
+    end
+
+  end
+
+
 
 
 end
