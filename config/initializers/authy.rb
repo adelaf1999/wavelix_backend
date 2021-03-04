e805d6c395f7467c533fbaf4602ddd6647c39196
Adel <adelwaboufakher@gmail.com>
2021-03-04 13:27:09 +0200

    Refactored the code that fetches the authy api key in authy.rb to fetch for the key depending if environment is development or production

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,2 +1,2 @@
-Authy.api_key = ENV.fetch('AUTHY_API_KEY')
+Authy.api_key = Rails.env.development? ? ENV.fetch('DEVELOPMENT_AUTHY_API_KEY')  :  ENV.fetch('PRODUCTION_AUTHY_API_KEY')
 Authy.api_uri = 'https://api.authy.com/'
