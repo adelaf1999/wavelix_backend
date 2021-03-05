ac0a0fe63b7860404bc975879bfcc57b91b65721
Adel <adelwaboufakher@gmail.com>
2021-03-05 14:26:38 +0200

    Configured email for production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
 
-  default from: 'Wavelix'
+  default from: "Wavelix <#{ Rails.env.development? ? ENV.fetch('DEVELOPMENT_EMAIL_ALIAS') : ENV.fetch('PRODUCTION_EMAIL_ALIAS')  }>"
+
+  default reply_to: '<>'
 
   layout 'mailer'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'wavelix.com', protocol: 'https' }
+
+  config.action_mailer.smtp_settings = {
+      user_name:      ENV.fetch('PRODUCTION_EMAIL'),
+      password:       ENV.fetch('PRODUCTION_EMAIL_PASSWORD'),
+      domain:         'wavelix.com',
+      address:       'smtp.office365.com.',
+      port:          '587',
+      authentication: :login,
+      enable_starttls_auto: true
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
