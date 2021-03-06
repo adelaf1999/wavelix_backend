ec59e4436e9b6f2f44abc860adda6cdfeaf4cd2c
Adel <adelwaboufakher@gmail.com>
2021-03-06 10:06:03 +0200

    Modulized all mailer previews inside the mailers directory to work with Zeitwerk naming convention

--- app/mailers/previews/driver_mailer_preview.rb
+++ app/mailers/previews/driver_mailer_preview.rb
@@ -1,9 +1,13 @@
-class DriverMailerPreview < ActionMailer::Preview
+module Previews
 
-  def account_verified
-    email = 'adelwaboufakher@gmail.com'
-    driver_name = 'Wajih Abou Fakher'
-    DriverMailer.account_verified(email, driver_name)
+  class DriverMailerPreview < ActionMailer::Preview
+
+    def account_verified
+      email = 'adelwaboufakher@gmail.com'
+      driver_name = 'Wajih Abou Fakher'
+      DriverMailer.account_verified(email, driver_name)
+    end
+
   end
 
 end
--- app/mailers/previews/unconfirmed_order_mailer_preview.rb
+++ app/mailers/previews/unconfirmed_order_mailer_preview.rb
@@ -1,34 +1,39 @@
-class UnconfirmedOrderMailerPreview < ActionMailer::Preview
+module Previews
 
-  def notify_customer_order_canceled
-    email = 'adelwaboufakher@gmail.com'
-    customer_name = 'Adel Abou Fakher'
-    store_name = 'Nike'
-    UnconfirmedOrderMailer.notify_customer_order_canceled(email, customer_name, store_name)
+  class UnconfirmedOrderMailerPreview < ActionMailer::Preview
+
+    def notify_customer_order_canceled
+      email = 'adelwaboufakher@gmail.com'
+      customer_name = 'Adel Abou Fakher'
+      store_name = 'Nike'
+      UnconfirmedOrderMailer.notify_customer_order_canceled(email, customer_name, store_name)
+    end
+
+    def notify_store_order_canceled
+      email = 'adelwaboufakher@gmail.com'
+      customer_name = 'Adel Abou Fakher'
+      UnconfirmedOrderMailer.notify_store_order_canceled(email, customer_name)
+    end
+
+    def notify_admin_unconfirmed_order
+      email = 'adelwaboufakher@gmail.com'
+      admin_name = 'Adel Abou Fakher'
+      order_id = 1
+      UnconfirmedOrderMailer.notify_admin_unconfirmed_order(email, admin_name, order_id)
+    end
+
+
+    def notify_admin_order_canceled
+      email = 'adelwaboufakher@gmail.com'
+      admin_name = 'Wajih Abou Fakher'
+      customer_name = 'Adel Abou Fakher'
+      store_name = 'ShopSmart'
+      order_id = 1
+      UnconfirmedOrderMailer.notify_admin_order_canceled(email, admin_name, customer_name, store_name, order_id)
+    end
+
+
   end
-
-  def notify_store_order_canceled
-    email = 'adelwaboufakher@gmail.com'
-    customer_name = 'Adel Abou Fakher'
-    UnconfirmedOrderMailer.notify_store_order_canceled(email, customer_name)
-  end
-
-  def notify_admin_unconfirmed_order
-    email = 'adelwaboufakher@gmail.com'
-    admin_name = 'Adel Abou Fakher'
-    order_id = 1
-    UnconfirmedOrderMailer.notify_admin_unconfirmed_order(email, admin_name, order_id)
-  end
-
-
-  def notify_admin_order_canceled
-    email = 'adelwaboufakher@gmail.com'
-    admin_name = 'Wajih Abou Fakher'
-    customer_name = 'Adel Abou Fakher'
-    store_name = 'ShopSmart'
-    order_id = 1
-    UnconfirmedOrderMailer.notify_admin_order_canceled(email, admin_name, customer_name, store_name, order_id)
-  end
-
 
 end
+
