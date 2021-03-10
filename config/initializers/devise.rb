ec447150281e5b4f0245d8856698b0e287ac33bd
Adel <adelwaboufakher@gmail.com>
2021-03-10 11:55:08 +0200

    Set the parent class responsible for sending emails as ApplicationMailer in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,13 +18,13 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV.fetch("DEVELOPMENT_EMAIL")
+  # config.mailer_sender = Rails.env.development? ? ENV.fetch('DEVELOPMENT_EMAIL_ALIAS') : ENV.fetch('PRODUCTION_EMAIL_ALIAS')
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
